14382cadaa91ac991e46f364d2f6432a2f5b268c
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-06 14:46:51 +0100

    Suppression et déplacement de fichiers

--- timer.js
+++
@@ -1,8 +0,0 @@
-Minuteur minute seconde nen Javascript
-
-Données de réservation stockées dans navigateur à l'aide de l'API Html5 WebStorage et affichées dans footer en dessous du panneau. 
-Affiche en permanence l'état de la réservation (s'il y en a) avec un décompte dynamique du temps restant avant expiration de la réservation 
-
-setTimeout (function(){alert("Vous avez dépassé 20 min...Réessayez !")});
-
- reset the time after a short delay setTimeout(function() { event.target.time = ""; }, 500); }, false);
