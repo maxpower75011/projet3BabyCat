453fe5bfda3be39a5fd11b0797184ddde4923133
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-06-08 15:15:45 +0200

    Indentation + bugs

--- js/map.js
+++ js/map.js
@@ -1,68 +1,67 @@
-//Récupérer dans le fichier Json la latitude et longitude pour chacun des vélos et l'injecter dans la variable marker pour les afficher sur sur la map
-var mymap;
-var data;
-var ville = 'Nantes';
+//Récupérer ds fichier Json latitude/longitude pour chacun des vélos et injecter ds la variable marker pour afficher sur sur la map
+	var mymap;
+	var data;
+	var ville = 'Nantes';
 
 function init_map() {
 
-//Initialisation de la carte
-   var mymap = L.map('mapid').setView([47.217, -1.553], 13.5);
+	//Initialisation de la carte
+   	var mymap = L.map('mapid').setView([47.217, -1.553], 13.5);
 
-   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+   	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
       maxZoom: 18,
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       id: 'mapbox.streets'
-   }).addTo(mymap);
+    }).addTo(mymap);
 
-//Recuperation des infos via API JC Decaux
-   var url = 'https://api.jcdecaux.com/vls/v1/stations?contract='+ ville +'&apiKey=0ae98a85f566e50063913e55f95b55366337fba6';
-   var oXhr = new XMLHttpRequest();
+	//Recuperation des infos via API JC Decaux
+	var url = 'https://api.jcdecaux.com/vls/v1/stations?contract='+ ville +'&apiKey=0ae98a85f566e50063913e55f95b55366337fba6';
+    var oXhr = new XMLHttpRequest();
 
-   oXhr.onload = function() {
-      var data = jQuery.parseJSON(this.responseText);
-//data est un tableau qui contient toutes les stations
-// ici les données sont exploitables
-      console.log('retour : ', data);
-//console.log('nom : ', data[9].name, 'lat : ', data[9].position.lat, 'lng : ', data[9].position.lng);
+   	oXhr.onload = function() {
+    var data = jQuery.parseJSON(this.responseText);
+	//data est un tableau qui contient toutes les stations
+	// ici les données sont exploitables
+    console.log('retour : ', data);
+	//console.log('nom : ', data[9].name, 'lat : ', data[9].position.lat, 'lng : ', data[9].position.lng);
 
-// Création des marker et positionnement sur la carte
-      var marker = []; //
-      var i;
-      for (i = 0; i < data.length; i++) {
-		 data[i].reserved = 'NON';
-//if(data[i].status != 'CLOSED') { //Afficher que les stations ouvertes
-         marker[i] = new L.Marker([data[i].position.lat, data[i].position.lng]).addTo(mymap);
-//console.log(marker[i]);
-         marker[i].bindPopup('status:'+data[i].status + '<br/>' + data[i].name + '<br/> Vélo reservé : ' + data[i].reserved +  '<br/> Vélos dispos : ' + data[i].available_bikes + '<br/> <p id="velov"> ID VELO' + data[i].number + '</p> <br/>          <div class="bouton">              <input onclick="onResaClick(this)" id="resa" data-velov="' + data[i].number + '" type="submit" value="Réserver"/>          </div>');
-// corriger le input type pour enlever le onclick $('#resa').on('click', onResaClick);
-//marker[i].on('click', onClick);
-// pour faire quelquechose au clic sur le marker //}
+	// Création des marker et positionnement sur la carte
+	var marker = []; //
+	var i;
+	for (i = 0; i < data.length; i++) {
+		data[i].reserved = 'NON';
+	//if(data[i].status != 'CLOSED') { //Afficher que les stations ouvertes
+        marker[i] = new L.Marker([data[i].position.lat, data[i].position.lng]).addTo(mymap);
+	//console.log(marker[i]);
+        marker[i].bindPopup('status:'+data[i].status + '<br/>' + data[i].name + '<br/> Vélo reservé : ' + data[i].reserved +  '<br/> Vélos dispos : ' + data[i].available_bikes + '<br/> <p id="velov"> ID VELO' + data[i].number + '</p> <br/>          <div class="bouton">              <input onclick="onResaClick(this)" id="resa" data-velov="' + data[i].number + '" type="submit" value="Réserver"/>          </div>');
+	// corriger le input type pour enlever le onclick $('#resa').on('click', onResaClick);
+	//marker[i].on('click', onClick);
+	// pour faire quelquechose au clic sur le marker //}
       }
    };
 
-   oXhr.onerror = function(data) {
-      console.log('Erreur ...');
-   }
-   oXhr.open('GET', url, true);
-   oXhr.send(null);
+ 	oXhr.onerror = function(data) {
+		console.log('Erreur ...');
+ 	}
+	oXhr.open('GET', url, true);
+	oXhr.send(null);
 }
 
 function onClick(e) {
-	// pour faire quelquechose au clic sur le marker
+	// pour faire qqch au clic sur marker
     //e.openPopup();
     //alert('ok');
 }
 
 function onResaClick(e) {
-  //console.log($( "#number" ).innerText);
-   //e.openPopup();
+	//console.log($( "#number" ).innerText);
+	//e.openPopup();
 	var idvelov = e.getAttribute('data-velov');
-   //console.log(velov);
+	//console.log(velov);
 	$( "#register" ).css( "display", "block" );
     $( "#velov-register" ).val(idvelov);
-
 }
 
 $(document).ready(function() {
--- js/webstorage.js
+++ js/webstorage.js
@@ -17,15 +17,15 @@ document.getElementById('stockage').onclick = function() {
 };
 	
 // Méthode de lecture
-document.getElementById('lecture').onclick = function() {
-	if(typeof localStorage!='undefined' && JSON) {
+//document.getElementById('lecture').onclick = function() {
+//	if(typeof localStorage!='undefined' && JSON) {
 // Si le local storage est supporté alors création d'une variable coordonnée qui récupère les infos stockées dans le local storage
-		var coordonnees = JSON.parse(localStorage.getItem('coord'));
+//		var coordonnees = JSON.parse(localStorage.getItem('coord'));
 		
-		document.getElementById('nom').value = coordonnees.nom;
-		document.getElementById('prenom').value = coordonnees.prenom;
-		document.getElementById('velov-register').value = coordonnees.velov;
-		alert("Lecture effectuée");
-	} else alert("localStorage n'est pas supporté");
+//		document.getElementById('nom').value = coordonnees.nom;
+//		document.getElementById('prenom').value = coordonnees.prenom;
+//		document.getElementById('velov-register').value = coordonnees.velov;
+//		alert("Lecture effectuée");
+//	} else alert("localStorage n'est pas supporté");
 // Sinon affiche un message d'erreur
-};
+//};
