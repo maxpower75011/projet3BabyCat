eb7e47e171de98ab6c8302ca2adf95a4536df1d1
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-06-09 23:23:12 +0200

    s'occuper de l'appel Json et à terme enlever le Json dans dossiers

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,31 @@
-//Il faut faire un appel Ajax par rapport à l'API JC Decaux et utliser Real Time Data en mettant Nantes dans l'appel --> Utiliser Fetch ?
+//appel Ajax API JC Decaux, Real Time Data, Nantes dans l'appel --> Fetch ?
+
+GET https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={api_key}
+
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
+function afficher(reponse) {
+    console.log(reponse);
+}
+
+//fonction AJAX générique
+ajaxGet(" https://api.jcdecaux.com/vls/v1/stations?contract={contract_Nantes}&apiKey={api_key}", afficher);
+
+//retrouver clé d'API associée à mon compte et l'insérer dans api_key
